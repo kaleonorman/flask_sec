bf556425e792ac13a01166f3a1be47f7f553cfc5
kaleonorman <kaleonorman@gmail.com>
2018-03-19 21:44:37 -1000

    added flash, redirect, and url_for

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -13,7 +13,11 @@ def index():
 def register():
     return render_template('register.html', title="Registration")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Logan requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index')) #redirect to home page if username/password correct
+
     return render_template('login.html', title="Sign In", form=form)
