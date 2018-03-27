b3dbb31cc3099644d2cc6e93f8c4fa61006bc2a3
kaleonorman <kaleonorman@gmail.com>
2018-03-26 21:42:54 -1000

    updated models.py, routes.py, and __init__.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### Start virtual environment
-```source env/bin/activate```
+```source .env/bin/activate```
 
 ### Freeze iinstalled modules
 - Run everytime you install a new module to keep up to date
@@ -24,11 +24,25 @@
 ```python manage.py db upgrade```
 
 ##Python ripl
+``` from app import db```
+```>>> from app.models import User```
+```>>> user = User(username='kaleo', email='k@gmail.com')```
+```>>> user.set_password('pass')```
+```>>> db.session.add(user)```
+```>>> db.session.commit()```
+
 ```from app import db```
 ```from app.models import User```
 ```users = User.query.all()```
 ```for user in users: print(user.username)```
 ```for user in users: print(user.password_hash)```
+
+```pip install flask-login```
+``` pip freeze > requirements.txt```
+
+
+
+
 
 ##psql
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, flash, redirect, url_for
 from app import app
+from flask_login import current_user, login_user
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index', methods= ['GET', 'POST'])
@@ -15,8 +17,17 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+            
         flash('Logan requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index')) #redirect to home page if username/password correct
 
